5f15395073ea8545429a92046aeb04ff7866d23c
Waltonr <waltonr18126@gmail.com>
2022-04-14 17:27:08 -0400

    Updated ProductSerializer Class and Added a New Field + Updated Product Class

--- products/models.py
+++ products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8,decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.CharField(max_length=255, default='')
+
+    
